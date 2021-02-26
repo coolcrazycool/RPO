000b6ab2fd7101e8d7e4108a2275c989812d137b
coolcrazycool <diman-rraazz@mail.ru>
2021-02-26 13:56:56 +0300

    Lab2 is done

--- moneyGrabber/app/src/main/java/com/millioner/moneygrabber/MainActivity.java
+++ moneyGrabber/app/src/main/java/com/millioner/moneygrabber/MainActivity.java
@@ -4,18 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
+        initRng();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        byte[] rnd = randomBytes(16);
+        byte[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        byte[] encrypted = encrypt(rnd, data);
+        byte[] decrypted = decrypt(rnd, encrypted);
+        String originalData  = new String(data, StandardCharsets.UTF_8);
+        String encryptedData = new String(encrypted, StandardCharsets.UTF_8);
+        String decryptedData = new String(decrypted, StandardCharsets.UTF_8);
+
+        System.out.println(originalData);
+        System.out.println(encryptedData);
+        System.out.println(decryptedData);
+
+        String output = new String(
+                "Original: "  + originalData  + "\n" +
+                        "Encrypted: " + encryptedData + "\n" +
+                        "Decrypted: " + decryptedData + "\0"
+        );
+
+        System.out.println("Original: "  + originalData);
+        System.out.println("Encrypted: " + encryptedData);
+        System.out.println("Decrypted: " + decryptedData);
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
@@ -27,4 +52,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
